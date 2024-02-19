771e5ab858056919156c19fb10bc288284a2b151
Monique Mendes <moniiquemendes96@gmail.com>
2024-02-19 20:21:18 +0100

    Ejercicio Mappea un array de objetos para pintarlos

--- NODE/jsx-basics/src/App.jsx
+++ NODE/jsx-basics/src/App.jsx
@@ -1,13 +1,14 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-//import './App.css'
+import './App.css'
 
 const App= () => {
 
   const x = 9;
 
-  
+  const datos = ["Monique", "27 anos", "Brasileña", "Granada-ES"];
+
   return (
     <>
      {x >= 6 && x < 12 ?(
@@ -17,6 +18,10 @@ const App= () => {
      ): (
       <p>Buenas Noches</p>
      )}
+    {datos.map((dato, index) => (
+  <p key={index}>{dato}</p>
+))}
+<button></button>
     </>
   )
 }; 
