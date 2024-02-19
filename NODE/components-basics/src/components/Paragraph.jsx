84285cf1ef07d6bde33348ac8a12e6a34ca8df25
Monique Mendes <moniiquemendes96@gmail.com>
2024-02-20 00:46:18 +0100

    Ejercicio Componente Paragraph

---
+++ NODE/components-basics/src/components/Paragraph.jsx
@@ -0,0 +1,9 @@
+
+
+const Paragraph = () => {
+  return (
+    <p> La simplicidad se encuentra con el poder, transformando ideas en interfaces dinámicas e intuitivas</p>
+  )
+}
+
+export default Paragraph
