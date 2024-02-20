ff75c6f2e04ab81f1abab83bcb482464ac3d7fef
Monique Mendes <moniiquemendes96@gmail.com>
2024-02-20 09:23:34 +0100

    Ejercicio Componentes Props Teste añadiendo + SubTittle

--- NODE/components-props-basics/src/App.jsx
+++ NODE/components-props-basics/src/App.jsx
@@ -3,17 +3,19 @@
 // import viteLogo from '/vite.svg'
 import Title from "./components/Title";
 import SubTittle from "./components/SubTitle";
-import Image from "./components/Image"
+import Image from "./components/Image";
 import './App.css'
 
 const App = ()=> {
   const nombre = "Haciendo el teste con props";
-  const subtitle = "Haciendo el segundo teste con props"
+  const subtitle1 = "Haciendo el segundo teste con props";
+  const subtitle2 = "Soy mas uno Teste Mona";
 
   return (
     <>
     <Title nombre={nombre}/>
-    <SubTittle subtitle= {subtitle}/>
+    <SubTittle subtitle= {subtitle1}/>
+    <SubTittle subtitle= {subtitle2}/>{/* Segunda vez usando SubTitle */}
     <Image/>
     </>
   )
