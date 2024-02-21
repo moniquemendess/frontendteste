0780db84410ca63c0badaee85d3a66cc6daee9d0
Monique Mendes <moniiquemendes96@gmail.com>
2024-02-21 17:28:53 +0100

    Ejercio Rick Morty completo

--- NODE/mini-proyecto-rick-morty/src/App.jsx
+++ NODE/mini-proyecto-rick-morty/src/App.jsx
@@ -2,20 +2,7 @@
 import React, { useState } from 'react';
 import './App.css'
 useState
-/**
- * Creamos un array de objectos que simula los datos de la api
- */
-const charactersMock = [{
-  id:1,
-  name:"Rick",
-  status: "Alive",
-},{
-  id: 2,
-  name:"Morty Smith",
-  status: "Alive",
-},
 
-];
 
 const App = () =>{
   /** 
@@ -23,8 +10,19 @@ const App = () =>{
    * Eso es un destruturing 
    * Re
   */
-  const [characterList, setCharacterList] = <React className="useState"></React>(charactersMock);
- 
+  const [characterList, setCharacterList] = React.useState([]);
+
+
+  React.useEffect(() => {
+    (async () => {
+      let data = await fetch(`https://rickandmortyapi.com/api/character/`).then(
+        (res) => res.json()
+      );
+  
+      setCharacterList(data.results);
+    }  )   ();
+  }, []);
+
   /** <> fragmentos pai 
    * Recorer el array com map.
    * React nos obliga que pusimos el key para guardar los cada alemento del elemento por key={character.id} 
@@ -32,10 +30,12 @@ const App = () =>{
   return (
     <>
       {characterList.map((character)=> (
+
         <div key={character.id}>
-          <h2>id:{character.id}</h2>
+          
           <h2>name:{character.name}</h2>
-          <h2>status:{character.status}</h2>
+         
+          <img src={character.image} alt="Characters" />
         </div>
       ))}
   
