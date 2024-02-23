9c6ae5f3a4b8fdd2b094a8f592fe92c9942db7d8
Monique Mendes <moniiquemendes96@gmail.com>
2024-02-23 10:10:58 +0100

    UseState

---
+++ NODE/useStateOne/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { MyState } from './components'
+
+
+const App=  () => {
+
+
+  return (
+    <>
+   <MyState/>
+    </>
+  )
+}
+
+export default App
---
+++ NODE/useStateOne/src/components/MyState/MyState.jsx
@@ -0,0 +1,26 @@
+/**
+ * Es un componente de tipo función que devuelve elementos de tipo react Element
+ * Crear un estado para ese compontente de getter y setter para pintar o modificar el estado 
+ * El Ziggy Stardust es el valor por defecto del estado
+ * OnChance es el evento donde si for cambio el estado del myName, sera llamada la function
+ * con un setName con novo valor de caampo
+ * 
+ */ 
+
+import { useState } from "react"
+
+export const MyState = ()=> {
+    const [myName, setMyName] = useState("Ziggy Stardust")
+  return (
+    <>
+    <h1>{myName}</h1>
+    <input 
+    type="text" 
+    value={myName}
+    onChange={(e) => setMyName(e.target.value)}
+    
+    />
+    
+    </>
+  )
+}
